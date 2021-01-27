f4f97ca6c6254901b64046f2cb58940868040449
hui <291856618@qq.com>
2021-01-27 11:42:12 +0800

    update webpack config file

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -52,8 +52,8 @@ const appPackageJson = require(paths.appPackageJson);
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 
-// const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
-const shouldUseSourceMap = false;
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
+// const shouldUseSourceMap = false;
 
 const webpackDevClientEntry = require.resolve(
   'react-dev-utils/webpackHotDevClient'
